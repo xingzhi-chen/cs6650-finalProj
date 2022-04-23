d9cc54f6bac9d39b0f8892e260debe7bfca070bf
syche55 <syche55@gmail.com>
2022-04-22 20:03:31 -0700

    init login server

--- src/main/java/server/login/LoginServer.java
+++ src/main/java/server/login/LoginServer.java
@@ -1,13 +1,27 @@
 package server.login;
 
 import com.sun.net.httpserver.HttpServer;
+import server.config.DBHelper;
+import server.config.ServerConfig;
+import server.route.RouteServerInterface;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class LoginServer {
+    private static DBHelper dbHelper;
+    private static final String host = "127.0.0.1";
+
+    public LoginServer() throws NotBoundException, RemoteException {
+        dbHelper = new DBHelper();
+    }
     private boolean ifUsernameExist(String username) {
         return true;
     }
@@ -18,14 +32,18 @@ public class LoginServer {
 
     public static void main(String[] args) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-            server.createContext("/login", new LoginHandler());
-            server.createContext("/register", new RegisterHandler());
+            LoginServer loginServer = new LoginServer();
+
+            HttpServer server = HttpServer.create(new InetSocketAddress(8090), 0);
+            server.createContext("/login", new LoginHandler(dbHelper));
+            server.createContext("/register", new RegisterHandler(dbHelper));
 
             ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
             server.setExecutor(threadPoolExecutor);
             server.start();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/server/login/RegisterHandler.java
+++ src/main/java/server/login/RegisterHandler.java
@@ -2,6 +2,7 @@ package server.login;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import server.config.DBHelper;
 
 import java.io.IOException;
 
@@ -9,6 +10,9 @@ import java.io.IOException;
  * Handler for user register request
  */
 public class RegisterHandler implements HttpHandler {
+    public RegisterHandler(DBHelper dbHelper) {
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
